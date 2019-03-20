6ad93bd82a520a606900352127243d0c85732398
Eric <eric.mcgarry@mail.utoronto.ca>
2019-03-20 16:19:21 -0400

    changes for testing

--- main.v
+++ main.v
@@ -8,6 +8,7 @@ module sampler (
     assign reset = 0;
     assign q, w, e, r, t, y, u, i, o, p, a, s, d, f, g, h;
     assign left, right, up, down;
+    reg [2:0] out;
     keyboard #(PULSE_OR_HOLD(1)) keyboard_tracker(
         .clock(CLOCK_50),
         .reset(reset),
@@ -37,27 +38,32 @@ module sampler (
         .down(down)
     );
     
-    assign select = {q, w, e, r, t, y, u, i, o, p, a, s, d, f, g, h};
+    assign select <= 0000000000000000;
     
-    always @(CLOCK_50)
+    always @(posedge CLOCK_50)
+    begin    
+        select =  = {q, w, e, r, t, y, u, i, o, p, a, s, d, f, g, h}
+    end
+    
+    always @(posedge CLOCK_50)
     begin
         case(select)
-            1000000000000000: //q_output
-            0100000000000000: //w_output
-            0010000000000000: //e_output
-            0001000000000000: //r_output
-            0000100000000000: //t_output
-            0000010000000000: //y_output
-            0000001000000000: //u_output
-            0000000100000000: //i_output
-            0000000010000000: //o_output
-            0000000001000000: //p_output
-            0000000000100000: //a_output
-            0000000000010000: //s_output
-            0000000000001000: //d_output
-            0000000000000100: //f_output
-            0000000000000010: //g_output
-            0000000000000001: //h_output
+            16'b1000000000000000: out = 3'b001;//q_output
+            16'b0100000000000000: out = 3'b010;//w_output
+            16'b0010000000000000: out = 3'b100;//e_output
+            16'b0001000000000000: out = 3'b110;//r_output
+            16'b0000100000000000: out = 3'b011;//t_output
+            16'b0000010000000000: //y_output
+            16'b0000001000000000: //u_output
+            16'b0000000100000000: //i_output
+            16'b0000000010000000: //o_output
+            16'b0000000001000000: //p_output
+            16'b0000000000100000: //a_output
+            16'b0000000000010000: //s_output
+            16'b0000000000001000: //d_output
+            16'b0000000000000100: //f_output
+            16'b0000000000000010: //g_output
+            16'b0000000000000001: //h_output
         endcase
     end
 		
