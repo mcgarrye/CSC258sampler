4b4f549eca117e4320ea6651115f59d1325942ed
McGarry Eric <mcgarrye@teach.cs.toronto.edu>
2019-04-03 13:18:18 -0400

    Addition of possible rate divider

--- main.v
+++ main.v
@@ -9,8 +9,8 @@ module main (
     
     wire q, w, e, r, t, y, u, i, o;
     wire left, right, up, down;
-	 wire [27:0] enable;
-	 reg cenable;
+    wire clk2;
+
 	
     keyboard_tracker #(.PULSE_OR_HOLD(0)) keyboard(
         .clock(CLOCK_50),
@@ -32,6 +32,12 @@ module main (
         .right(right), 
         .up(up), 
         .down(down)
+    );
+
+    clk_div new_clk(
+	.clk(CLOCK_50),
+	.reset(SW[6]),
+	.clk_out(clk2)
     );
 	 
     
@@ -56,7 +62,7 @@ module main (
 		counter1 <= 0;
 		counter2 <= 0;
 		end
-	if (SW[4:3] == 4'b00 b) 
+	if (SW[4:3] == 4'b00) 
  		begin 
  		seqtemp1 <= seq1; 
  		seqtemp2 <= seq2; 
@@ -151,5 +157,50 @@ module main (
 		seqtemp2 <= seqtemp2<<9;
 		end
     end
+
+    reg tester;
+    always @(negedge clk2)
+    begin
+	case(tester)
+		1'b0: tester <= 1'b1;
+		1'b1: tester <= 1'b0;
+		default: tester <= 1'b0;
+	endcase
+    end
+
     assign LEDR = out;   
+endmodule
+
+
+module clk_div (clk, reset, clk_out);
+     
+    input clk;
+    input reset;
+    output clk_out;
+     
+    reg [1:0] r_reg;
+    wire [1:0] r_nxt;
+    reg clk_track;
+     
+    always @(posedge clk or posedge reset)
+     
+    begin
+      if (reset)
+         begin
+            r_reg <= 3'b0;
+    	clk_track <= 1'b0;
+         end
+     
+      else if (r_nxt == 2'b10)
+     	   begin
+    	     r_reg <= 0;
+    	     clk_track <= ~clk_track;
+    	   end
+     
+      else 
+          r_reg <= r_nxt;
+    end
+     
+     assign r_nxt = r_reg+1;   	      
+     assign clk_out = clk_track;
 endmodule
